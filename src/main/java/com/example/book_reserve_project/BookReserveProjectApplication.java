6369abc47e10adae3f37126c6df66149aeeb2fbb
te230467 <te230467@gmail.com>
2026-06-26 11:37:31 +0900

    最初のコミット

---
+++ src/main/java/com/example/book_reserve_project/BookReserveProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.book_reserve_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookReserveProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookReserveProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/book_reserve_project/BookReserveProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.book_reserve_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookReserveProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
